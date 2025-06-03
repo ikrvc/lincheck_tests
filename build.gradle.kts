f80ed5c90ea837c871f08d2fd7f9c4a5502cf502
Jamila Seyidova <jamilaseidova@gmail.com>
2025-06-03 22:50:40 +0200

    Update LinCheck and JVM toolchain for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,15 @@ dependencies {
     testImplementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlinx:lincheck:2.28")
+    testImplementation("org.jetbrains.kotlinx:lincheck:2.39")
     testImplementation("junit:junit:4.13")
 }
 
 kotlin {
-    jvmToolchain(19)
+    jvmToolchain(21)
 }
 
 tasks.withType<Test> {
     jvmArgs( "--add-opens", "java.base/java.lang=ALL-UNNAMED", "--add-opens", "java.base/jdk.internal.vm=ALL-UNNAMED", "--add-opens", "java.base/jdk.internal.access=ALL-UNNAMED", "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED", "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED", "--add-exports", "java.base/sun.security.action=ALL-UNNAMED" )
     maxHeapSize = "16g"
 }
-
